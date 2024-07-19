5ee36e41c90b4349d691d9ffc961dc6e78c2ace0
klortiz13 <kris.ortiz13alv@gmail.com>
2024-07-19 15:34:52 -0700

    added comments and renamed pa5 to pa4

--- pa4-starter/src/PartitionOracle.java
+++ pa4-starter/src/PartitionOracle.java
@@ -19,11 +19,11 @@ public class PartitionOracle {
      * 
      * p.partition(someArray, someLow, someHigh)
      * 
-     * @param p
-     * @param strs
-     * @param low
-     * @param high
-     * @return
+     * @param p - the pivot
+     * @param strs - the array
+     * @param low - the low
+     * @param high - the high
+     * @return - a definetly invalid pivot (-1)
      */
     public static int runPartition(Partitioner p, String[] strs, int low, int high) {
         try {
@@ -36,14 +36,34 @@ public class PartitionOracle {
     // The three methods below are for you to fill in according to the PA writeup.
     // Feel free to make other helper methods as needed.
 
+    /**
+     * @Return null if the pivot and after array reflect a correct partitioning of 
+     * the before array between low and high.
+     * 
+     * @Return a non-null String (your choice) describing why it isn't a valid
+     * partition if it is not a valid result. 
+     * 
+     * You might choose Strings like these, though there may be more you want to report:
+     * - "after array doesn't have same elements as before"
+     * - "Item before pivot too large"
+     * - "Item after pivot too small"
+    */
     public static String isValidPartitionResult(String[] before, int low, int high, int pivot, String[] after) {
         return null;
     }
 
+    /**
+     * Generate a list of items of size n
+     */
     public static String[] generateInput(int n) {
         return new String[0];
     }
 
+    /**
+     * 
+     * @return - null for any reasonable good partition implementation.
+     * @return - a CounterExample for any bad partition implementation.
+     */
     public static CounterExample findCounterExample(Partitioner p) {
         return null;
     }
--- pa4-starter/src/TestPartitionOracle.java
+++ pa4-starter/src/TestPartitionOracle.java
@@ -31,3 +31,5 @@ public class TestPartitionOracle {
     }
 
 }
+
+// Test your tester below
